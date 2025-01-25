cca5e83beb7965f215dd6cfc41311acad28547ed
karllogdat <logdatk@gmail.com>
2025-01-26 00:36:03 +0800

    add very minimal constructor

--- src/main/java/com/hexacode/EntryPanel.java
+++ src/main/java/com/hexacode/EntryPanel.java
@@ -1,5 +1,7 @@
 package com.hexacode;
 
+import java.awt.*;
+
 import javax.swing.*;
 
 public class EntryPanel extends JPanel {
@@ -14,5 +16,21 @@ public class EntryPanel extends JPanel {
 
     private void setupUI() {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setAlignmentX(LEFT_ALIGNMENT);
+
+        cbxDone = new JCheckBox("Mark as Done");
+        cbxDone.setSelected(entry.isDone());
+
+        // make these like the ones above,
+        // assign new JLabels to the declared fields of the class
+        add(new JLabel(entry.getName()));
+        add(new JLabel(entry.getSubject()));
+        add(new JLabel(entry.getType().toString()));
+        add(new JLabel(entry.getDeadline().toString()));
+    }
+
+    @Override
+    public Dimension getMaximumSize() {
+        return new Dimension(Integer.MAX_VALUE, getPreferredSize().height);
     }
 }
